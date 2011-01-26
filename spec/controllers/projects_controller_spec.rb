662d0b2804224fb7f9c6f4efb8930eb013a8fabc
Jim Schubert <james.schubert@gmail.com>
2011-01-26 07:31:06 -0800

    Added admin namespaced users controller

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -1,11 +1,7 @@
 require 'spec_helper'
 
 describe ProjectsController do
-  let(:user) do
-    user = Factory(:user)
-    user.confirm!
-    user
-  end
+  let(:user) { create_user! }
   let(:project) { Factory(:project) }
 
   it "displays an error message when asked for a missing project" do
